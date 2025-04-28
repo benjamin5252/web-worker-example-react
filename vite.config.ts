764d895e554c68011e651ba079c841c8c3931fe4
DavidYT Lan <DavidYT_Lan@cyberlink.com>
2025-04-28 22:45:42 +0800

    <feat>: modify the base url

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base:"/web-worker-example-react/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
