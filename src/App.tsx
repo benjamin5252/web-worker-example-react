76e927666d58c104ef0c0d9174421fd991ee5504
DavidYT Lan <DavidYT_Lan@cyberlink.com>
2025-04-28 22:50:17 +0800

    <fix>: sonic ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,7 @@
-import { useState } from 'react'
-import { Button } from "@/components/ui/button"
-import { Progress } from "@/components/ui/progress"
 import './App.css'
 import PageFreeze from './pages/pageFreeze'
 
 function App() {
-  const [count, setCount] = useState(0)
-  
-
   return (
     <>
       <div className="flex flex-col items-center justify-center min-h-svh">
